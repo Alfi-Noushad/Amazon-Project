13a2251de514c966b7300b081360d30f725b67bf
Alfi Noushad <alfinoushad@gmail.com>
2025-02-16 12:35:23 +0530

    Update checkout.js

--- script/checkout.js
+++ script/checkout.js
@@ -30,7 +30,7 @@ cart.forEach((cartItem) => {
           ${matchingProduct.name}
         </div>
         <div class="product-price">
-          $${matchingProduct.priceCents / 100}
+          $${formatCurrency(matchingProduct.priceCents)}
         </div>
         <div class="product-quantity">
           <span>
@@ -39,12 +39,63 @@ cart.forEach((cartItem) => {
           <span class="update-quantity-link link-primary">
             Update
           </span>
-          <span class="delete-quantity-link link-primary">
+          <span class="delete-quantity-link link-primary js-delete-link">
             Delete
           </span>
+        </div>
+         <div class="delivery-options">
+                <div class="delivery-options-title">
+                  Choose a delivery option:
+                </div>
+
+                <div class="delivery-option">
+                  <input type="radio" class="delivery-option-input"
+                    name="${matchingProduct.id}">
+                  <div>
+                    <div class="delivery-option-date">
+                      Tuesday, June 21
+                    </div>
+                    <div class="delivery-option-price">
+                      FREE Shipping
+                    </div>
+                  </div>
+                </div>
+            <div class="delivery-option">
+              <input type="radio" checked class="delivery-option-input"
+                name="${matchingProduct.id}">
+              <div>
+                <div class="delivery-option-date">
+                  Wednesday, June 15
+                </div>
+                <div class="delivery-option-price">
+                  $4.99 - Shipping
+                </div>
+              </div>
+            </div>
+            <div class="delivery-option">
+              <input type="radio" class="delivery-option-input"
+                name="${matchingProduct.id}">
+              <div>
+                <div class="delivery-option-date">
+                  Monday, June 13
+                </div>
+                <div class="delivery-option-price">
+                  $9.99 - Shipping
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 `;
 });
 
 document.querySelector(".order-summary").innerHTML = cartSummaryHTML;
+
+document.querySelectorAll(".js-delete-link")
+ .forEach((link) => {
+   link.addEventListener('click', () => {
+     
+   })
+ });
+
